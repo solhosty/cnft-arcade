59fcd1fa56c9ceada6c87bb74d8037f3f75981c2
hunter <104146303+solhosty@users.noreply.github.com>
2023-10-21 20:39:26 -0400

    new

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 </script>
 <svelte:head> <title>cNFT Arcade</title> </svelte:head>
 
-	<div class="w-full h-full bg-black">
+	<div class="w-full h-full bg-black overflow-hidden">
 		<img src="/minion.png" class="w-48 pt-10 m-auto" alt=""/>
 		{#if !$walletStore$?.connected}
 		<div>
